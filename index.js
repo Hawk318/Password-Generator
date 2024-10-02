a9e0307b52d293ac071432f09588989b7d6b5b29
Hawk318 <darylhawkins@yahoo.com>
2024-10-02 12:31:13 +0700

    Adding Files
    adding css, html, and js files

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passOne = document.getElementById("pass-1")
+let passTwo = document.getElementById("pass-2")
+
+function generatePassword() {
+    let x = ""
+    let y = ""
+    let passwordOne = ""
+    let passwordTwo = ""
+    for (let i=0; i < 15; i++) {
+          x = characters[Math.floor( Math.random() * characters.length )] 
+          passwordOne += x
+          passOne.textContent = passwordOne
+    }   for (let i=0; i < 15; i++) {
+            y = characters[Math.floor( Math.random() * characters.length )] 
+            passwordTwo += y
+            passTwo.textContent = passwordTwo
+        }
+        
+    
+    }
+
+
+
+
